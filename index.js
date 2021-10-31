411d78ce58d441bac1d5f5dac70f706658bfe24b
Robert Sefer <rsefer@gmail.com>
2021-10-30 19:22:09 -0500

    Refactored paths #1

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 #!/usr/bin/env node
-const pathConfig = require('path');
-const path = pathConfig.resolve(__dirname, '.');
-const parentPath = process.cwd();
-const ourPackage = require(process.cwd() + '/package.json');
+const path = require('path');
+const project = require('./lib/project.js');
 const argv = require('minimist')(process.argv.slice(2));
 const chokidar = require('chokidar');
 const glob = require('glob');
 
-const bustCache = require(path + '/lib/bustCache.js');
-const buildSass = require(path + '/lib/style.js');
-const buildJS = require(path + '/lib/scripts.js');
-const buildImages = require(path + '/lib/images.js');
-const buildFonts = require(path + '/lib/fonts.js');
-const buildBrowserSync = require(path + '/lib/browsersync.js');
+const bustCache = require('./lib/bustCache.js');
+const buildSass = require('./lib/style.js');
+const buildJS = require('./lib/scripts.js');
+const buildImages = require('./lib/images.js');
+const buildFonts = require('./lib/fonts.js');
+const buildBrowserSync = require('./lib/browsersync.js');
 
 let chokidarOpts = {
 	ignoreInitial: true
 };
 
 function bustFunctionsCache() {
-	bustCache(parentPath + '/functions.php');
+	bustCache(project.path + '/functions.php');
 }
 
 function frontrunImages() {
-	[parentPath + '/_src/images/', parentPath + '/_src/images/**/*/'].forEach((block) => {
+	[project.path + '/_src/images/', project.path + '/_src/images/**/*/'].forEach((block) => {
 		glob(block, {}, function(err, directory) {
 			directory.forEach((dir) => {
 				buildImages(dir);
@@ -33,10 +31,10 @@ function frontrunImages() {
 }
 
 let entries = {};
-for (const [name, files] of Object.entries(ourPackage.sdc.entries)) {
+for (const [name, files] of Object.entries(project.package.sdc.entries)) {
 	entries[name] = [];
 	files.forEach(function(file) {
-		entries[name].push(parentPath + file);
+		entries[name].push(project.path + file);
 	});
 }
 
@@ -44,7 +42,7 @@ let filesSass = [];
 
 for (const [name, files] of Object.entries(entries)) {
 	files.forEach(function(file) {
-		switch (pathConfig.parse(file).ext) {
+		switch (path.parse(file).ext) {
 			case '.scss':
 				filesSass.push(file);
 				break;
@@ -67,7 +65,7 @@ filesSass.forEach((file) => {
 	bustFunctionsCache();
 });
 if (argv.watch) {
-	chokidar.watch(parentPath + '/_src/style/**/*', chokidarOpts).on('all', (event, path) => {
+	chokidar.watch(project.path + '/_src/style/**/*', chokidarOpts).on('all', (event, path) => {
 		filesSass.forEach((file) => {
 			buildSass(file);
 			bustFunctionsCache();
@@ -76,12 +74,12 @@ if (argv.watch) {
 }
 frontrunImages()
 if (argv.watch) {
-	chokidar.watch(parentPath + '/_src/images/**/*', chokidarOpts).on('all', (event, path) => {
+	chokidar.watch(project.path + '/_src/images/**/*', chokidarOpts).on('all', (event, path) => {
 		frontrunImages();
 	});
 }
 
-buildFonts(parentPath + '/_src/fonts');
+buildFonts(project.path + '/_src/fonts');
 
 if (argv.watch) {
 	buildBrowserSync();
