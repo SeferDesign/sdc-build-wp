af8ad4cc861e65fffd6d24dde8d4d31cfed89673
Robert Sefer <rsefer@gmail.com>
2021-11-05 15:45:13 -0500

    Style linting

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "rules": {
+    "indentation": "tab",
+    "max-empty-lines": 1,
+    "rule-empty-line-before": [
+      "always",
+      {
+        "ignore": [
+          "after-comment"
+        ]
+      }
+    ],
+    "color-no-invalid-hex": true
+  }
+};
