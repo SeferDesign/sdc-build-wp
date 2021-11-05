e64e9d614ce7fd50861107e348ad9daafced09fd
Robert Sefer <rsefer@gmail.com>
2021-11-04 19:51:28 -0500

    eslint with autofix

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"parserOptions": {
+		"ecmaVersion": 6,
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"jsx": true
+		}
+	},
+	"rules": {
+		"semi": 1,
+		"indent": [1, "tab"],
+		"no-multiple-empty-lines": [
+			1,
+			{
+				"max": 1
+			}
+		]
+	}
+};
